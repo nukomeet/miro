06972af8accf193d1d22c10232b449dafeb20fc5
Łukasz Jan Niemier <lukasz@niemier.pl>
2016-08-19 16:35:20 +0200

    Force coercion to string

--- lib/miro.rb
+++ lib/miro.rb
@@ -1,24 +1,25 @@
-require "miro/version"
-require "cocaine"
-require "color"
-require "tempfile"
-require "open-uri"
-require png_lib = (RUBY_ENGINE != "jruby" ? "oily_png" : "chunky_png") # Load the C extension oily_png unless jruby is the platform in use.
+require 'miro/version'
+require 'cocaine'
+require 'color'
+require 'tempfile'
+require 'open-uri'
+require(RUBY_ENGINE != 'jruby' ? 'oily_png' : 'chunky_png') # Load the C extension oily_png unless jruby is the platform in use.
 
-require "miro/dominant_colors"
+require 'miro/dominant_colors'
 
 module Miro
   class << self
     def options
-      convert = `which convert`.strip
+      convert = `which convert`.to_s.strip
       @options ||= {
-        :image_magick_path  => convert.length > 0 ? convert : '/usr/bin/convert',
-        :resolution         => "150x150",
-        :color_count        => 8,
-        :quantize           => 'RGB',
-        :method             => 'pixel_group'
+        image_magick_path: convert.empty? ? '/usr/bin/convert' : convert,
+        resolution: '150x150',
+        color_count: 8,
+        quantize: 'RGB',
+        method: 'pixel_group'
       }
     end
+
     def pixel_group?
       options[:method] == 'pixel_group'
     end
